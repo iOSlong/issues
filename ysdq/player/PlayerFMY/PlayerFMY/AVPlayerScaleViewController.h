f8ed6a5e03b0d783ad47bfa36390f40c6e74f6b3
龙学武 <longxuewu@tv365.net>
2018-06-26 19:19:10 +0800

    add player in ysdq

---
+++ ysdq/player/PlayerFMY/PlayerFMY/AVPlayerScaleViewController.h
@@ -0,0 +1,13 @@
+//
+//  AVPlayerScaleViewController.h
+//  PlayerFMY
+//
+//  Created by lxw on 2018/6/26.
+//  Copyright © 2018年 lxw. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface AVPlayerScaleViewController : BaseViewController
+
+@end
---
+++ ysdq/player/PlayerFMY/PlayerFMY/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  PlayerFMY
+//
+//  Created by lxw on 2018/6/26.
+//  Copyright © 2018年 lxw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
